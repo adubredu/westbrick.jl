ba012c79327854d733ba682f15b2f16971fe06b1
Alphonsus Adu-Bredu <alphonsusbq436@gmail.com>
2021-12-01 14:53:55 -0500

    fixed quat w

--- test/test_grippers.jl
+++ test/test_grippers.jl
@@ -1,6 +1,8 @@
 using Revise
 using bob 
 using MeshCat
+using StaticArrays
+using CoordinateTransformations
 
 robot, state, mvis = load_bob()
 render(mvis)
@@ -8,4 +10,10 @@ render(mvis)
 close_gripper!(robot, state, mvis)
 open_gripper!(robot, state, mvis)
 
-follow_trajectory!(robot, state, mvis, -pi)
+position = SVector(1., 0., 0.)
+settransform!(mvis.visualizer["world/base"], Translation(position))
+# settransform!(mvis.visualizer["world/rack"], Translation(position))
+
+
+# follow_trajectory!(robot, state, mvis, -pi)
+rotate_bob!(robot, state, mvis, pi, position)
