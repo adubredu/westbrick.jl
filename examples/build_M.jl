ba88184eb88e418b72f388d22f8cf226bdceb274
Alphonsus Adu-Bredu <alphonsusbq436@gmail.com>
2021-12-07 12:18:52 -0500

    bigm perfected

--- examples/build_M.jl
+++ examples/build_M.jl
@@ -1,7 +1,7 @@
 using Revise
 using bob 
 
-num_objects = 11 
+num_objects = 13 
 positions = [[i,j] for (i,j) in zip(rand(3:8, num_objects),rand(-8:-3, num_objects))]
 place_pose = [[-2, 2, 0.],[-2, 3, 0.],[-2, 4, 0.], [-2, 5, 0.],[-2, 6, 0.],[-1, 5, 0.],
               [0, 4, 0.],[1, 5, 0.],[2, 6, 0.],[2, 5, 0.],[2, 4, 0.],[2, 3, 0.],[2, 2, 0.]]
@@ -13,7 +13,7 @@ bobby = load_robot()
 traj, obj_traj = init_visualization()
 
 function lay_block!(i)
-    pose = [positions[i][1], positions[i][2]-0.5, -π/2.]
+    pose = [positions[i][1], positions[i][2]+0.5, -π/2.]
     move!(bobby, pose, traj, obj_traj)
     pick!(bobby, -π/2., objects[i], traj, obj_traj)
     move!(bobby, place_pose[i], traj, obj_traj)
@@ -27,4 +27,4 @@ for i = 1:num_objects
 end
 
 println("visualizing...")
-visualize_trajectory!(bobby, traj, obj_traj, obs_dict)
+visualize_trajectory!(bobby, traj, obj_traj, obs_dict;name="media/build_M.mp4")
